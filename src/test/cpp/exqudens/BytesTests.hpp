4101a61e8a6e871a58da29919e73f6ae424a7f97
exqudens <exqudens@gmail.com>
2022-01-02 20:30:27 -0800

    step 16

--- src/test/cpp/exqudens/BytesTests.hpp
+++ src/test/cpp/exqudens/BytesTests.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <format>
 #include <iostream>
 
 #include <type_traits>
@@ -13,16 +14,19 @@ namespace exqudens {
   TEST(BytesTests, longVectorConstReferenceToUnsignedCharVector) {
     const std::vector<long> expected = {123, -123, 987};
     for (size_t i = 0; i < expected.size(); i++) {
-      std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      //std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      std::cout << std::format("expected.at({}): '{}'", i, expected.at(i)) << std::endl;
     }
     const std::vector<unsigned char> bytes = Bytes::toUnsignedCharVector(expected);
     for (size_t i = 0; i < bytes.size(); i++) {
-      std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      //std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      std::cout << std::format("bytes.at({}): '{}'", i, +bytes.at(i)) << std::endl;
     }
     ASSERT_EQ(size_t(12), bytes.size());
     const std::vector<long> actual = Bytes::toLongVector(bytes);
     for (size_t i = 0; i < actual.size(); i++) {
-      std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      //std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      std::cout << std::format("actual.at({}): '{}'", i, actual.at(i)) << std::endl;
     }
     ASSERT_EQ(expected, actual);
   }
@@ -30,16 +34,19 @@ namespace exqudens {
   TEST(BytesTests, longVectorReferenceToUnsignedCharVector) {
     std::vector<long> expected = {123, -123, 987};
     for (size_t i = 0; i < expected.size(); i++) {
-      std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      //std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      std::cout << std::format("expected.at({}): '{}'", i, expected.at(i)) << std::endl;
     }
     std::vector<unsigned char> bytes = Bytes::toUnsignedCharVector(expected);
     for (size_t i = 0; i < bytes.size(); i++) {
-      std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      //std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      std::cout << std::format("bytes.at({}): '{}'", i, +bytes.at(i)) << std::endl;
     }
     ASSERT_EQ(size_t(12), bytes.size());
     std::vector<long> actual = Bytes::toLongVector(bytes);
     for (size_t i = 0; i < actual.size(); i++) {
-      std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      //std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      std::cout << std::format("actual.at({}): '{}'", i, actual.at(i)) << std::endl;
     }
     ASSERT_EQ(expected, actual);
   }
@@ -47,7 +54,8 @@ namespace exqudens {
   TEST(BytesTests, longVectorConstPointerToUnsignedCharVector) {
     const std::vector<long> expected = {123, -123, 987};
     for (size_t i = 0; i < expected.size(); i++) {
-      std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      //std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      std::cout << std::format("expected.at({}): '{}'", i, expected.at(i)) << std::endl;
     }
     const std::vector<long>* expectedPointer = nullptr;
     std::vector<unsigned char> bytes = Bytes::toUnsignedCharVector(expectedPointer);
@@ -56,7 +64,8 @@ namespace exqudens {
     expectedPointer = &expected;
     bytes = Bytes::toUnsignedCharVector(expectedPointer);
     for (size_t i = 0; i < bytes.size(); i++) {
-      std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      //std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      std::cout << std::format("bytes.at({}): '{}'", i, +bytes.at(i)) << std::endl;
     }
     ASSERT_EQ(size_t(12), bytes.size());
 
@@ -67,7 +76,8 @@ namespace exqudens {
     bytesPointer = &bytes;
     actual = Bytes::toLongVector(bytesPointer);
     for (size_t i = 0; i < actual.size(); i++) {
-      std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      //std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      std::cout << std::format("actual.at({}): '{}'", i, actual.at(i)) << std::endl;
     }
     ASSERT_EQ(expected, actual);
   }
@@ -75,7 +85,8 @@ namespace exqudens {
   TEST(BytesTests, longVectorPointerToUnsignedCharVector) {
     std::vector<long> expected = {123, -123, 987};
     for (size_t i = 0; i < expected.size(); i++) {
-      std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      //std::cout << "expected.at(" << i << "): '" << expected.at(i) << "'" << std::endl;
+      std::cout << std::format("expected.at({}): '{}'", i, expected.at(i)) << std::endl;
     }
     std::vector<long>* expectedPointer = nullptr;
     std::vector<unsigned char> bytes = Bytes::toUnsignedCharVector(expectedPointer);
@@ -84,7 +95,8 @@ namespace exqudens {
     expectedPointer = &expected;
     bytes = Bytes::toUnsignedCharVector(expectedPointer);
     for (size_t i = 0; i < bytes.size(); i++) {
-      std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      //std::cout << "bytes.at(" << i << "): '" << +bytes.at(i) << "'" << std::endl;
+      std::cout << std::format("bytes.at({}): '{}'", i, +bytes.at(i)) << std::endl;
     }
     ASSERT_EQ(size_t(12), bytes.size());
 
@@ -95,7 +107,8 @@ namespace exqudens {
     bytesPointer = &bytes;
     actual = Bytes::toLongVector(bytesPointer);
     for (size_t i = 0; i < actual.size(); i++) {
-      std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      //std::cout << "actual.at(" << i << "): '" << actual.at(i) << "'" << std::endl;
+      std::cout << std::format("actual.at({}): '{}'", i, actual.at(i)) << std::endl;
     }
     ASSERT_EQ(expected, actual);
   }
