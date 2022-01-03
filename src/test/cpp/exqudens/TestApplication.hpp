ce7d2924472ff51f9785c9bebc8472f9f52ca8c7
exqudens <exqudens@gmail.com>
2022-01-02 20:43:01 -0800

    step 18

--- src/test/cpp/exqudens/TestApplication.hpp
+++ src/test/cpp/exqudens/TestApplication.hpp
@@ -2,6 +2,8 @@
 
 #include "gtest/gtest.h"
 
+#include "exqudens/BytesTests.hpp"
+
 namespace exqudens {
 
   class TestApplication {
--- src/test/cpp/main.cpp
+++ src/test/cpp/main.cpp
@@ -1,5 +1,4 @@
 #include "exqudens/TestApplication.hpp"
-#include "exqudens/BytesTests.hpp"
 
 int main(int argc, char** argv) {
   return exqudens::TestApplication::run(&argc, argv);
